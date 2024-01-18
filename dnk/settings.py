cdeee557a3f2b7dd9728a3420b6386f5d6c6381a
lionel <ndabagajeanlionel@gmail.com>
2024-01-18 15:08:24 +0200

    Created another app to hold everything related to index of the app

--- dnk/settings.py
+++ dnk/settings.py
@@ -122,6 +122,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, STATIC_URL),
 ]
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
