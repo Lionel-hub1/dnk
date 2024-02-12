fa39cfe273622437db1e123496b1c1988977a975
Jean Lionel Ndabaga <78383037+Lionel-hub1@users.noreply.github.com>
2024-02-12 15:44:50 +0200

    Add BannerAd model to Django admin and modify admin panel header

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from .models import Genre, Product, Color
+from .models import Genre, Product, Color, BannerAd
 
 admin.site.register(Genre)
 admin.site.register(Product)
 admin.site.register(Color)
+admin.site.register(BannerAd)
+
+# Other Django Admin Modifications
+admin.site.site_header = "DNK Store Admin Panel"
